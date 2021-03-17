edc2d4e9b436f89b66e0545399aec247df1ffbf0
Sivaji Yelugoti <sivajiyelugoti@Sivajis-MBP.broadband>
2021-03-17 20:56:22 +0000

    modified files

--- src/main/java/com/example/demo/controller/DemoApplication.java
+++ src/main/java/com/example/demo/controller/DemoApplication.java
@@ -16,6 +16,6 @@ public class DemoApplication {
 
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        return String.format("Hello User  %s!", name);
     }
 }

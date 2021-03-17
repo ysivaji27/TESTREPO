9d2c3c2d89e5f6ad3d9c268cdd2208ed450f0772
Sivaji Yelugoti <sivajiyelugoti@Sivajis-MBP.broadband>
2021-03-17 21:02:13 +0000

    new change

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println(" Start the Application ");
+		System.out.println(" Startoing ..... ");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
